979be8b1d128baa1a2bd1bcfe072760adeb7d0db
rjb19179 <rjb19179@uni.strath.ac.uk>
2020-03-12 17:06:34 +0000

    Updated and finished FavouritesList class.

--- artemis/app/src/main/java/com/example/artemis/backend/FavouritesList.java
+++ artemis/app/src/main/java/com/example/artemis/backend/FavouritesList.java
@@ -1,30 +1,58 @@
 package com.example.artemis.backend;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.example.artemis.MainActivity;
+import com.google.gson.Gson;
 import java.util.ArrayList;
 
 /*
  * Class for list of favourites.
  */
 public class FavouritesList {
-    private ArrayList<Favourite> favourites;
+    private SharedPreferences fav;
 
     public FavouritesList() {
-        ArrayList<Favourite> favourites = new ArrayList<Favourite>();
+
     }
 
     /*
-     * Adds a favourite to the list.
+     * 	Adds a favourite to the list. To save to sharedPreferences, must convert arraylist to gson:
      */
-    public void addFavourite(String title, String url) {
-        Favourite fav = new Favourite(title, url);
-        favourites.add(fav);
+    public void addToFavourites(String title, String url) {
+        Favourite favId = new Favourite(title, url);
+        SharedPreferences fav = MainActivity.this.getSharedPreferences();
+        //Retrieve ArrayList from preferences:
+        Gson gson = new Gson();
+        String json = fav.getString("Favourites", "");
+        ArrayList<Favourite> list = gson.fromJson(json, ArrayList.class);
+
+        list.add(favId);
+        //Add ArrayLit back to preferences:
+        Gson gsonAdd = new Gson();
+        String jsonAdd = gson.toJson(list);
+        SharedPreferences.Editor editor = fav.edit();
+        editor.putString("Favourites", json);
+        editor.commit();
     }
 
     /*
      * Removes a favourite from the list.
      */
-    public void removeFavourite(Favourite fav) {
-        favourites.remove(fav);
+    public void removeFavourite(Favourite favId) {
+        SharedPreferences fav = MainActivity.this.getSharedPreferences();
+        //Retrieve ArrayList from preferences:
+        Gson gson = new Gson();
+        String json = fav.getString("Favourites", "");
+        ArrayList<Favourite> list = gson.fromJson(json, ArrayList.class);
+
+        list.remove(favId);
+        //Add ArrayLit back to preferences:
+        Gson gsonAdd = new Gson();
+        String jsonAdd = gson.toJson(list);
+        SharedPreferences.Editor editor = fav.edit();
+        editor.putString("Favourites", json);
+        editor.commit();
     }
 
 }

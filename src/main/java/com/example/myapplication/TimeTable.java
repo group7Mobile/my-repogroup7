1046fc1d94afe9c966cf39023f82551d12919a20
JagdeepGill5 <60473317+JagdeepGill5@users.noreply.github.com>
2020-03-16 12:07:13 +0000

    Update TimeTable.java

--- src/main/java/com/example/myapplication/TimeTable.java
+++ src/main/java/com/example/myapplication/TimeTable.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TimeTable extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TimeTable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table);
+    }
+
+    public void mainPage(View v) {
+        Intent goMainPage = new Intent(TimeTable.this, Settings.class);
+        startActivity(goMainPage);
     }
 }

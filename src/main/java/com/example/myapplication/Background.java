26bffb736d13ec0aa2017c59ba52bd5f7d9e810a
JagdeepGill5 <60473317+JagdeepGill5@users.noreply.github.com>
2020-03-16 12:04:57 +0000

    Update Background.java

--- src/main/java/com/example/myapplication/Background.java
+++ src/main/java/com/example/myapplication/Background.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.example.artemis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Background extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Background extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_background);
+    }
+
+    public void mainPage(View v) {
+        Intent goMainPage = new Intent(Background.this, Settings.class);
+        startActivity(goMainPage);
     }
 }
